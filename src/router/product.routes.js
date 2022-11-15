d45f6dfde785595c728329443d7f9f2c6e2112f4
KevinQL1 <kevinquintero045@gmail.com>
2022-11-15 16:42:28 -0500

    Trabajo final

---
+++ src/router/product.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { deleteOneProductController, getAllProductsController,getOneProductController, getProductsByCategoryController, postNewProduct } from "../controller/productsController.js";
+import { logginMiddleware } from "../middleware/logginMiddleware.js";
+
+const router = Router()
+
+router.get('/all', logginMiddleware, getAllProductsController)
+router.get('/:id', logginMiddleware, getOneProductController)
+router.get('/category/:category', logginMiddleware, getProductsByCategoryController)
+router.post('/', logginMiddleware, postNewProduct )
+router.post('/delete/:product_id', logginMiddleware, deleteOneProductController)
+
+export default router
